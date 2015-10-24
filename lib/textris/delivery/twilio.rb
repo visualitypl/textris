ef6aaaf7c4c0b1fdb977054e65c9365a85610bc0
Konrad Głowacki <konrad@webkrak.pl>
2015-10-24 22:19:17 +0200

    Add plus sign in phone numbers for Twillio delivery

--- lib/textris/delivery/twilio.rb
+++ lib/textris/delivery/twilio.rb
@@ -3,12 +3,17 @@ module Textris
     class Twilio < Textris::Delivery::Base
       def deliver(to)
         client.messages.create(
-          :from => message.from_phone,
-          :to   => to,
+          :from => phone_with_plus(message.from_phone),
+          :to   => phone_with_plus(to),
           :body => message.content)
       end
 
       private
+
+      # Twillo requires phone numbers starting with a '+' sign
+      def phone_with_plus(phone)
+        phone.to_s.start_with?('+') ? phone : "+#{phone}"
+      end
 
       def client
         @client ||= ::Twilio::REST::Client.new
