3f41fd140c5abeb01a910a2d016c64ae5bd0d2a8
yossi <yossimotro@gmail.com>
2016-08-03 11:57:07 +0300

    Calling Twilio's Client.account to send SMS.

--- lib/textris/delivery/twilio.rb
+++ lib/textris/delivery/twilio.rb
@@ -2,7 +2,7 @@ module Textris
   module Delivery
     class Twilio < Textris::Delivery::Base
       def deliver(to)
-        client.messages.create(
+        client.account.messages.create(
           :from => phone_with_plus(message.from_phone),
           :to   => phone_with_plus(to),
           :body => message.content)
