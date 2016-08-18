688d1b45a25af74db5f07717aa97d09c86fcb45d
Phil Nash <philnash@gmail.com>
2016-08-18 21:13:09 +0100

    Rails 5 ships with a default async queue.
    The Async queue responds to enqueue_at without raising a NotImplementedError.

--- spec/textris/delay/active_job_spec.rb
+++ spec/textris/delay/active_job_spec.rb
@@ -94,9 +94,9 @@ describe Textris::Delay::ActiveJob do
         job = MyTexter.delayed_action('48111222333').deliver_later(:queue => :custom)
         expect(job.queue_name).to eq 'custom'
 
-        expect do
-          MyTexter.delayed_action('48111222333').deliver_later(:wait => 10)
-        end.to raise_error(NotImplementedError)
+        now = Time.now.to_i
+        job = MyTexter.delayed_action('48111222333').deliver_later(:wait => 10)
+        expect(job.scheduled_at).to be >= now + 10
       end
 
       it 'executes job properly' do
