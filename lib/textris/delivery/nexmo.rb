b7af5b562388a12d6aca5227eafac152b2cef1f2
Paul Williams <paul@pdwilliams.net>
2018-01-22 19:54:48 +1100

    Nexmo deliveries can use alphanumeric "from" attribute

--- lib/textris/delivery/nexmo.rb
+++ lib/textris/delivery/nexmo.rb
@@ -3,7 +3,7 @@ module Textris
     class Nexmo < Textris::Delivery::Base
       def deliver(phone)
         client.send_message(
-          from: message.from_phone,
+          from: sender_id,
           to:   phone,
           text: message.content
         )
@@ -12,6 +12,10 @@ module Textris
       private
         def client
           @client ||= ::Nexmo::Client.new
+        end
+
+        def sender_id
+          message.from_phone || message.from_name
         end
     end
   end
